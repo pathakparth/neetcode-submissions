8b661375ca00e01edea1f52cd35af6efd50a76b0
Parth Pathak <39071878+pathakparth@users.noreply.github.com>
2026-09-08 09:06:17 -0400

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} coins
+     * @param {number} amount
+     * @return {number}
+     */
+    coinChange(coins, amount) {
+
+        const dpArr = new Array(amount + 1).fill(amount + 1);
+        dpArr[0] = 0;
+
+        for (let a = 1; a <= amount; a++) {
+            for (const coin of coins) {
+                if (coin <= a) {
+                    dpArr[a] = Math.min(dpArr[a], 1 + dpArr[a - coin]);
+                }
+            }
+        }
+        
+
+        return dpArr[amount] > amount ? -1 : dpArr[amount];
+    }
+}
