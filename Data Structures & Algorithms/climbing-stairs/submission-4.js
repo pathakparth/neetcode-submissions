eeecf683beb6d015e4369e84a95522d0bf80f3d5
Parth Pathak <39071878+pathakparth@users.noreply.github.com>
2026-09-07 23:12:30 -0400

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+
+    dpHelperMemo(n, arr) { // Memoization
+        if (n === 1 || n === 2) {
+            return n;
+        }
+
+        if (arr[n] !== -1) {
+            return arr[n];
+        }
+        
+        return arr[n] = this.dpHelperMemo(n-1, arr) + this.dpHelperMemo(n-2, arr);
+    }
+
+    climbStairs(n) { // Tabulation
+        const arr = new Array(n+1).fill(-1);
+
+        arr[1] = 1;
+        arr[2] = 2;
+
+        if (n === 1 || n === 2) {
+            return n;
+        }
+
+        for (let i = 3; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        return arr[n];
+    }
+}
